a60014a0b1b6592d7d7872f4a8d45c433862836d
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2025-10-17 19:42:53 +0300

    authentication task_2

--- college/views.py
+++ college/views.py
@@ -4,34 +4,47 @@ from college.models import Lesson, Course
 from college.serializers import LessonSerializer, CourseSerializer
 from rest_framework.permissions import IsAuthenticated
 
+from users.permissions import IsModerator
+
 
 class LessonCreateAPIView(CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, ~IsModerator]
 
 
 class LessonListAPIView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModerator]
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsModerator, IsAuthenticated]
 
 
 class LessonUpdateAPIView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsModerator, IsAuthenticated]
 
 
 class LessonDestroyAPIView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, ~IsModerator]
 
 
 class CourseViewSet(ModelViewSet):
     """Реализация CRUD для курса через Viewset"""
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action in ["create", "destroy"]:
+            self.permission_classes = (~IsModerator,)
+        elif self.action in ["update", "retrieve"]:
+            self.permission_classes = (IsModerator,)
+        return super().get_permissions()
